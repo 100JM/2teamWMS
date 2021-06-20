66d48d8ed00d954e1dbc12b337a42b24478415d2
bjm_9 <bjm_9@DESKTOP-42DGMTK>
2021-06-21 01:38:56 +0900

    화면 구성 및 경로 지정 1차

--- 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController.java
+++ 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class releaseController {
+public class ReleaseController {
 	
 	@GetMapping("/releaseApproval")
 	public String releaseApproval(Model model) {
--- 2teamproject/src/main/java/com/cafe2team/controller/VehicleController.java
+++ 2teamproject/src/main/java/com/cafe2team/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.cafe2team.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class VehicleController {
@@ -17,5 +18,10 @@ public class VehicleController {
 	public String vehicleAdd(Model model) {
 		model.addAttribute("title", "차량등록");
 		return "vehicle/vehicleAdd";
+	}
+	
+	@PostMapping("/vehicleAdd")
+	public String vehicleAdd() {
+		return "redirect:/vehicleList";
 	}
 }
