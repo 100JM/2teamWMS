2df91dd20998b8d50decfbe53130466ec941012b
bjm_9 <bjm_9@DESKTOP-42DGMTK>
2021-06-21 01:34:00 +0900

    2team 백종민 화면 구성 및 경로 설정 1차

--- 2teamproject/src/main/java/com/cafe2team/controller/SafetyCheckController.java
+++ 2teamproject/src/main/java/com/cafe2team/controller/SafetyCheckController.java
@@ -3,6 +3,7 @@ package com.cafe2team.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SafetyCheckController {
@@ -17,5 +18,16 @@ public class SafetyCheckController {
 	public String safetyCheckAdd(Model model) {
 		model.addAttribute("title", "안전점검등록");
 		return "safe/safetyCheckAdd";
+	}
+	
+	@PostMapping("/safetyCheckAdd")
+	public String safetyCheckAdd() {
+		return "redirect:/safetyCheck";
+	}
+	
+	@GetMapping("/modifySafetyCheck")
+	public String modifySafetyCheck(Model model) {
+		model.addAttribute("title", "안전점검수정");
+		return "safe/modifySafetyCheck";
 	}
 }
---
+++ 2teamproject/src/main/java/com/cafe2team/controller/VehicleController.java
@@ -0,0 +1,21 @@
+package com.cafe2team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VehicleController {
+	
+	@GetMapping("/vehicleList")
+	public String vehicleList(Model model) {
+		model.addAttribute("title", "차량목록 조회");
+		return "vehicle/vehicleList";
+	}
+	
+	@GetMapping("/vehicleAdd")
+	public String vehicleAdd(Model model) {
+		model.addAttribute("title", "차량등록");
+		return "vehicle/vehicleAdd";
+	}
+}
